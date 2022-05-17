c2a606530a63feaeb2e9983782a6a1ce4247f5c0
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-05-17 15:48:55 -0700

    changed sendFile path, wrong procfile

--- server.js
+++ server.js
@@ -103,8 +103,8 @@ app.use((err, req, res, next) => {
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "/client/build/index.html"));
     });
 }
 
