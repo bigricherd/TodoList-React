6f7d9928b610afa690a95ee50d75e7c61837fd07
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-06-02 00:53:03 -0700

    remove logging

--- server.js
+++ server.js
@@ -126,9 +126,6 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "/client/build/index.html"));
     });
-    console.log(corsConfig.origin);
-    console.log(sessionConfig.cookie);
-    console.log(sessionConfig.store);
 }
 
 const port = process.env.PORT || 5000;
