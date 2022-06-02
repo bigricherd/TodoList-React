0b89e0bf22a5df0a80f9cddb2b4839374fda43cd
Chino Rodriguez <chino@Chinos-MacBook-Pro.local>
2022-06-02 00:38:36 -0700

    dummy changes cuz I changed env variable

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ if (process.env.NODE_ENV === "production") {
     console.log(sessionConfig.store);
 }
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 })
